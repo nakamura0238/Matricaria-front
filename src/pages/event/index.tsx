e9403d83acd6c38cc0ea308c40d34db32414bdee
nakamura0238 <fumiya.nakamura0108@gmail.com>
2024-11-21 23:02:31 +0900

    feat: コンテンツのないページでリダイレクトさせる

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,35 +1,7 @@
-import { useEffect } from "react";
-import { Link, Outlet } from "react-router-dom";
-import { flexCol } from "./event.css";
-
-const ViewedEventKey = "viewedUUIDList"
-
-const toJson = (str: string) => {
-  return JSON.parse(str)
-}
-const toStr = (str: Object) => {
-  return JSON.stringify(str)
-}
+import {Navigate} from "react-router-dom";
 
 export const Event = () => {
-
-  let viewedEventList: Array<{UUID: string, EventName: string}> = []
-  const UUIDListText = localStorage.getItem(ViewedEventKey)
-  if (UUIDListText) {
-    viewedEventList = toJson(UUIDListText)
-  } 
-
   return (
-    <div>
-      <h1>Eventページ</h1>
-      <div className={flexCol}>
-        {viewedEventList.map((val, i) => {
-          return (
-            <Link key={i} to={`/event/${val.UUID}`}>{val.EventName}</Link>
-          )
-        })}
-      </div>
-      <Outlet />
-    </div>
+      <Navigate to={"/"} />
   );
 };
